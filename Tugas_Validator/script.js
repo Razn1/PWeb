a20798574890b1c8f50e3fd0e4170c326b0a3734
Razn1 <itoushura12@gmail.com>
2024-12-13 08:27:11 +0700

    Minor Update

--- Tugas_Validator/script.js
+++ Tugas_Validator/script.js
@@ -1,5 +1,5 @@
 function Validasi() {
-    let isValid = true;
+    let Valid = true;
 
     document.getElementById('errNama').textContent = '';
     document.getElementById('errMail').textContent = '';
@@ -9,39 +9,39 @@ function Validasi() {
 
     const nama = document.getElementById('nama').value.trim();
     if (!nama || nama.length > 30) {
-        isValid = false;
+        Valid = false;
         document.getElementById('errNama').textContent = 'Nama wajib diisi dan maksimal 30 karakter.';
     }
 
     const email = document.getElementById('email').value.trim();
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!email || !emailPattern.test(email)) {
-        isValid = false;
-        document.getElementById('errMail').textContent = 'Format email tidak isValid.';
+        Valid = false;
+        document.getElementById('errMail').textContent = 'Format email tidak Valid.';
     }
 
     const jam = document.getElementById('jam').value.trim();
     if (!jam) {
-        isValid = false;
+        Valid = false;
         document.getElementById('errJam').textContent = 'Jam keberangkatan wajib diisi.';
     }
 
     const tujuan = document.getElementById('tujuan').value.trim();
     if (!tujuan) {
-        isValid = false;
+        Valid = false;
         document.getElementById('errTujuan').textContent = 'Tujuan keberangkatan wajib diisi.';
     }
 
     const tiket = document.getElementById('tiket').value.trim();
     if (!tiket || tiket < 1 || tiket > 10) {
-        isValid = false;
+        Valid = false;
         document.getElementById('errTiket').textContent = 'Jumlah tiket harus antara 1 dan 10.';
     }
 
     const pesanBerhasil = document.getElementById('success');
-    if (isValid) {
+    if (Valid) {
         pesanBerhasil.style.display = 'block';
-        pesanBerhasil.innerHTML = `<strong>Data TerisValidasi!</strong><br>
+        pesanBerhasil.innerHTML = `<strong>Data TerValidasi!</strong><br>
             Nama: ${nama}<br>
             Email: ${email}<br>
             Jam Keberangkatan: ${jam}<br>
